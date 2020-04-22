2b9b4370ac6b7d4d3edadb76ffdf5ea903fdf35e
Justin Gonzalez <jfgonzalez99@gmail.com>
2020-04-22 15:11:17 -0400

    Removed unnecessary packages

--- pseudo_poly.py
+++ pseudo_poly.py
@@ -1,15 +1,19 @@
 import numpy as np
-from math import floor
 from random import randint
-from collections import Counter
 
 def numPartition(A):
     """ 
     Dynamic programming pseudo-polynomial time algorithm for the number 
     partition problem. Runs in O(nb) where n is the length of A and b is the 
     sum of A's elements.
+
+    Input:
+    A : list of non-negative integers
+    
+    Returns:
+    S : list of signs such that S[i] = 1 implies A[i] is in group 1 and 
+        S[i] = -1 implies A[i] is in group 2
     """
-    A = sorted(A)
     n = len(A)
     b = sum(A)
     D = np.array([[-1] * (n+1)] * (b+1))
@@ -29,44 +33,51 @@ def numPartition(A):
             elif D[i - A[j-1], j-1] != -1:
                 D[i,j] = i - A[j-1]
 
-    optimal = b/2
-    best_r = 2*b
+    min_res = b
     best_i = 0
-    # Find minimum residual
+    best_j = 0
+
+    # Find minimum residue
     for i in range(b+1):
         for j in range(n+1):
             if D[i,j] != -1:
-                r = abs(i-optimal)
-                if r < best_r:
-                    best_r = r
+                r = abs(i-(b-i))
+                if r < min_res:
+                    min_res = r
                     best_i = i
+                    best_j = j
 
     # Follow path to form A1
-    A1 = list(reversed(unroll(D,best_i,A)))
-    A2 = list((Counter(A) - Counter(A1)).elements())
-
+    S = unroll(D,best_i,best_j,n)
+    A1 = []
+    A2 = []
+    for i in range(n):
+        if S[i] == 1:
+            A1.append(A[i])
+        else:
+            A2.append(A[i])
+    
+    # Print results
+    print("S:", S)
+    print("Residue:", min_res)
+    print()
     print("A1:", A1)
     print("Sum(A1):", best_i)
     print()
     print("A2:", A2)
     print("Sum(A2):", b - best_i)
-    print()
-    print("Residual:", best_r)
 
-def unroll(R,i,A):
-    S = []
-    j = 0
-    while R[i,j] == -1:
-        j += 1
+def unroll(R,i,j,n):
+    S = [-1]*n
+    S[j-1] = 1
     while R[i,j] > 0:
-        if i != R[i,j]:
-            S.append(A[j-1])
         i = R[i,j]
         j = j-1
-    S.append(A[j-1])
+        if i != R[i,j]:
+            S[j-1] = 1
     return S
 
-A = [randint(0,9) for i in range(8)]
+A = [randint(0,9) for i in range(9)]
 print("A:", A)
 print()
 numPartition(A)
